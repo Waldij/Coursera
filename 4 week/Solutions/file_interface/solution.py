b0ae2cb10c5421e776a9d8d27839cd7e2ffefc19
Vlad <golublad@mail.ru>
2020-10-10 16:35:43 +0300

    File class solution

---
+++ 4 week/Solutions/file_interface/solution.py
@@ -0,0 +1,46 @@
+import tempfile
+import os.path
+
+class File():
+	"""docstring for ClassName"""
+
+	def __init__(self, path_to_file):
+		self.path_to_file = path_to_file
+		if not os.path.exists(self.path_to_file):
+			with open(self.path_to_file, 'w'):
+				pass
+
+	def read(self):
+		with open(self.path_to_file, 'r') as file:
+			return file.read()
+
+	def write(self, string_to_write):
+		with open(self.path_to_file, 'w') as file:
+			file.write(string_to_write)
+
+	def writeline(self, string_to_write):
+		with open(self.path_to_file, 'a') as file:
+			file.write(string_to_write)
+
+	def __add__(self, *args):
+
+		files_content = []
+		files_content.append(self.read())
+		for obj in args:
+			files_content.append(obj.read())
+
+		with tempfile.NamedTemporaryFile(delete = False) as temp:
+			new_file = File(temp.name)
+
+			for content in files_content:
+				new_file.writeline(content)
+
+		return new_file 
+
+	def __str__(self):
+		return self.path_to_file
+
+	def __getitem__(self, index):
+		with open(self.path_to_file, 'r') as file:
+			lines = list(file)
+			return lines[index]
